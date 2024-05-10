88b58eef2e49f3a05b92d02f1f74974f1c460b36
virus1705 <66819640+virus1705@users.noreply.github.com>
2024-05-10 14:52:39 +0300

    Update yandex_gpt.py

--- yandex_gpt.py
+++ yandex_gpt.py
@@ -10,7 +10,6 @@ logging.basicConfig(filename=LOGS,
                     format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s",
                     filemode="w")
 
-# ïîäñ÷èòûâàåì êîëè÷åñòâî òîêåíîâ â ñîîáùåíèÿõ
 def count_gpt_tokens(messages):
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
     headers = {
@@ -24,10 +23,10 @@ def count_gpt_tokens(messages):
     try:
         return len(requests.post(url=url, json=data, headers=headers).json()['tokens'])
     except Exception as e:
-        logging.error(e)  # åñëè îøèáêà - çàïèñûâàåì å¸ â ëîãè
+        logging.error(e)  # если ошибка - записываем её в логи
         return 0
 
-# çàïðîñ ê GPT
+# запрос к GPT
 def ask_gpt(messages):
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
     headers = {
@@ -41,16 +40,17 @@ def ask_gpt(messages):
             "temperature": 0.7,
             "maxTokens": MAX_GPT_TOKENS
         },
-        "messages": SYSTEM_PROMPT + messages  # äîáàâëÿåì ê ñèñòåìíîìó ñîîáùåíèþ ïðåäûäóùèå ñîîáùåíèÿ
+        "messages": SYSTEM_PROMPT + messages  # добавляем к системному сообщению предыдущие сообщения
     }
     try:
         response = requests.post(url, headers=headers, json=data)
-        # ïðîâåðÿåì ñòàòóñ êîä
+        # проверяем статус код
         if response.status_code != 200:
             return False, f"Ошибка GPT. Статус-код: {response.status_code}", None
+        # если всё успешно - считаем количество токенов, потраченных на ответ, возвращаем статус, ответ, и количество токенов в ответе
         answer = response.json()['result']['alternatives'][0]['message']['text']
         tokens_in_answer = count_gpt_tokens([{'role': 'assistant', 'text': answer}])
         return True, answer, tokens_in_answer
     except Exception as e:
-        logging.error(e)  # åñëè îøèáêà - çàïèñûâàåì å¸ â ëîãè
-        return False, "Ошибка при обращении к GPT",  None
+        logging.error(e)  # если ошибка - записываем её в логи
+        return False, "Ошибка при обращении к GPT",  None 
