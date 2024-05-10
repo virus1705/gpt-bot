cdd126fb96388007ffeb44826a353469d1f0d24d
virus1705 <66819640+virus1705@users.noreply.github.com>
2024-05-10 18:36:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MAX_USER_TTS_SYMBOLS = 5_000
 MAX_USER_GPT_TOKENS = 2_000
 MAX_TTS_SYMBOLS = 500
 
-LOGS = 'logs.txt'  # файл для логов
+LOGS = f'{HOME_DIR}logs.txt'  # файл для логов
 DB_FILE = 'messages.db'  # файл для базы данных
 SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
                                             'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
