22c2e070110d3c8e48e0e845005ceca13fb4d28d
virus1705 <66819640+virus1705@users.noreply.github.com>
2024-05-10 17:50:51 +0300

    Update SpeechKit.py

--- SpeechKit.py
+++ SpeechKit.py
@@ -1,5 +1,5 @@
 import requests
-from creds.creds import get_creds  # модуль для получения токенов
+from creds import get_creds  # модуль для получения токенов
 
 IAM_TOKEN, FOLDER_ID = get_creds()
 
