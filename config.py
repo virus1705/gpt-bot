bb3c5212ad15be9e62e203dfbccc7acb016a96a1
virus1705 <66819640+virus1705@users.noreply.github.com>
2024-05-10 18:52:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ MAX_USERS = 3  # максимальное кол-во пользователей
 MAX_GPT_TOKENS = 120  # максимальное кол-во токенов в ответе GPT
 COUNT_LAST_MSG = 4  # кол-во последних сообщений из диалога
 
-LOGS = '/logs.txt'  # файл для логов
 DB_FILE = '/messages.db'  # файл для базы данных
 
 HOME_DIR = "/home/student/gpt-bot"
@@ -16,7 +15,7 @@ MAX_USER_TTS_SYMBOLS = 5_000
 MAX_USER_GPT_TOKENS = 2_000
 MAX_TTS_SYMBOLS = 500
 
-LOGS = f'{HOME_DIR}logs.txt'  # файл для логов
+LOGS = f'{HOME_DIR}\logs.txt'  # файл для логов
 DB_FILE = 'messages.db'  # файл для базы данных
 SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
                                             'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
