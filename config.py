4e9983acc1f15b8ef8cbed55736741474d8dbba7
virus1705 <66819640+virus1705@users.noreply.github.com>
2024-05-10 17:36:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,12 @@ MAX_USERS = 3  # максимальное кол-во пользователей
 MAX_GPT_TOKENS = 120  # максимальное кол-во токенов в ответе GPT
 COUNT_LAST_MSG = 4  # кол-во последних сообщений из диалога
 
-HOME_DIR = '/home/student/gpt-bot'  # путь к папке с проектом
 LOGS = f'{HOME_DIR}/logs.txt'  # файл для логов
 DB_FILE = f'{HOME_DIR}/messages.db'  # файл для базы данных
 
-IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'
-FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'
-BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'
+IAM_TOKEN_PATH = '/creds/iam_token.txt'
+FOLDER_ID_PATH = '/creds/folder_id.txt'
+BOT_TOKEN_PATH = '/creds/bot_token.txt'
 
 # лимиты для пользователя
 MAX_USER_STT_BLOCKS = 10
